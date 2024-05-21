5a09946661f3f566da890c21dc396979bf171155
Wojciech Kołtunowski <wojciech.koltunowski@modivo.com>
2024-05-21 12:10:00 +0200

    add java class

---
+++ src/main/java/DuplicateValidatorJava.java
@@ -0,0 +1,11 @@
+import com.falco.workshop.validation.Row;
+import com.falco.workshop.validation.RowValidator;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class DuplicateValidatorJava implements RowValidator {
+    @Override
+    public void validate(@NotNull List<Row> rows) {
+    }
+}
